8f07770d88722ae0ff0e013a0f5a9b25eeb3512a
Kristian Saomi <christiansaomii@gmail.com>
2023-10-21 15:04:12 +0200

    Adding a Typescript interface for the product and changing launch settings to 'localhost 5000'

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
+import { Product } from "./products";
 
 function App() {
-  const [product, setProduct] = useState([
-    { name: "product1", price: 100.0 },
-    { name: "product2", price: 200.0 },
-  ]);
+  const [product, setProduct] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/products")
+    fetch("http://localhost:5000/api/Products")
       .then((response) => response.json())
       .then((data) => setProduct(data));
   }, []);
@@ -16,8 +14,12 @@ function App() {
     setProduct((prevState) => [
       ...prevState,
       {
+        id: prevState.length + 101,
         name: "product" + (prevState.length + 1),
         price: prevState.length * 100 + 100,
+        brand: "some brand",
+        description: "some description",
+        pictureUrl: "http://picsum.photos/200",
       },
     ]);
   }
@@ -26,10 +28,11 @@ function App() {
     <>
       <div>
         <h1>Re-Store</h1>
-        {product.map((item) => (
-          <ul key={item.name}>
-            <li>name: {item.name}</li>
-            <li>price: {item.price}</li>
+        {product.map((product) => (
+          <ul key={product.id}>
+            <li>name: {product.name}</li>
+            <li>price: {product.price}</li>
+            <li>image: {product.pictureUrl}</li>
           </ul>
         ))}
         <button id="addingProduct" onClick={addProduct}>
