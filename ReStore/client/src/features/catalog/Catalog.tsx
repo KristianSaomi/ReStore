f67fdbd32633ce15661e0ba466f738b91b04c7e3
Kristian Saomi <christiansaomii@gmail.com>
2023-10-28 15:03:05 +0200

    Added toggle button for darktheme - End of section 3

--- ReStore/client/src/features/catalog/Catalog.tsx
+++ ReStore/client/src/features/catalog/Catalog.tsx
@@ -1,20 +1,19 @@
-import { Button } from "@mui/material";
-
 import { Product } from "../../app/models/products";
 import ProductList from "./ProductList";
+import { useEffect, useState } from "react";
 
-interface Props {
-  products: Product[];
-  addProduct: () => void;
-}
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-export default function Catalog({ products, addProduct }: Props) {
+  useEffect(() => {
+    fetch("http://localhost:5000/api/Products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <>
       <ProductList products={products} />
-      <Button variant="contained" onClick={addProduct}>
-        Add product
-      </Button>
     </>
   );
 }
