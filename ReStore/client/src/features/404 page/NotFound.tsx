0eecf515c9347ee05f24cdb935074145b499f006
Kristian Saomi <christiansaomii@gmail.com>
2023-11-27 07:46:19 +0100

    Adding pages and routes and page not found

---
+++ ReStore/client/src/features/404 page/NotFound.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export default function NotFound() {
+  return (
+    <>
+      <Typography variant="h2">Page cant be found</Typography>
+      <Typography variant="body1">Try searching for something els</Typography>
+    </>
+  );
+}
